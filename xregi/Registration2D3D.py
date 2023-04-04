9232f2805bd710078531a3085d3a401d31158b3d
jeremyzz830 <zjm731215@outlook.com>
2023-04-03 23:25:31 -0400

    update

--- xregi/Registration2D3D.py
+++ xregi/Registration2D3D.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils import *
 import pandas as pd
+from LandmarkDetector import SynthexDetector
 
 class Registration2D3D:
     def __init__(self, image: np.ndarray, ct_path: str, landmarks_3d: dict, intrinsic: np.ndarray):
@@ -15,3 +16,17 @@ class Registration2D3D:
         landmarks_3d=get_3d_landmarks(landmarks_3d_path, folder_type = "fcsv",label_idx=11)
         #intrinsic load from dicom?
         return cls(image_load,ct_path_load, landmarks_3d,intrinsic_load)
+    
+    def run_synthex(self):
+        syn = SynthexDetector(self.image)
+        
+        self.landmarks_2D = syn.detect()
+    
+    def run_xreg(self):
+        pass
+
+    def run(self):
+        self.run_synthex()
+        self.run_xreg()
+
+        
