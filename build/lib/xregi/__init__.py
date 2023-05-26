929ed44695c0639ba77d13eb535dfe77f5235940
jeremyzz830 <zjm731215@outlook.com>
2023-05-26 15:45:41 -0400

    clean up useless files

--- build/lib/xregi/__init__.py
+++
@@ -1,5 +0,0 @@
-from .landmark_detector import SynthexDetector, LandmarkDetector
-from .registration_solver import XregSolver, RegistrationSolver
-
-
-__all__ = ["SynthexDetector", "LandmarkDetector", "XregSolver", "RegistrationSolver"]
